da0519dca5c03cb182f69445aa1abc044543babf
AbdQoharr Olayiwola <olayiwolaabdqoharr226!gmail.com>
2026-01-17 23:41:32 -0800

    new-commit fix

--- src/Components/Button/AccientButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const AccientButton = () => {
-  return (
-    <button className='bg-purple'>Sign Up</button>
-  )
-}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <header />
+      <h1 className='text-purple text-3xl'>My name is Kola</h1>
     </div>
   )
 }
